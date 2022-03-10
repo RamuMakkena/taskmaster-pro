eaf9c09164bb87ae4c2c0968081bf0bd3793e767
Ramu Makkena <MRamuMailer@gmail.com>
2022-03-10 12:59:39 -0600

    Adding UI changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -126,7 +126,7 @@ $("#task-form-modal").on("shown.bs.modal", function () {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function () {
+$("#task-form-modal .btn-save").click(function () {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -161,10 +161,22 @@ $(".card .list-group").sortable({
   scroll: false,
   tolerance: "pointer",
   helper: "clone",
-  // activate: function(event){console.log("activate", this);},
-  // deactivate: function(event){console.log("deactivate", this);},
-  // over:function(event){console.log("over", this);},
-  // out:function(event){console.log("out", this);},
+  activate: function(event){
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
+  },
+  deactivate: function(event){
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
+  },
+  over:function(event){
+    $(event.target).addClass("dropover-active");
+    $(".bottom-trash").addClass("bottom-trash-active");
+  },
+  out:function(event){
+    $(event.target).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active");
+  },
   update: function (event) {
     var tempArr = [];
     $(this).children().each(function () {
@@ -206,6 +218,12 @@ var auditTask = function(taskEl){
     $(taskEl).addClass("list-group-item-warning");
   }
 };
+
+setTimeout(function(){
+  $(".card .list-group-item").each(function(index,el){
+    auditTask(el);
+  });
+  },1800000);
 
 // load tasks for the first time
 loadTasks();
